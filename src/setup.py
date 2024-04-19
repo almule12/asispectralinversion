f7eb3a52c9ab19c144ff76661716707f3be75f65
Alexander Mule <alexander.mule.gr@Dartmouth.edu>
2024-04-19 13:17:50 -0400

    removed temporary name change in setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='asispectralinversion2',
+    name='asispectralinversion',
     version='1.0.0',
     packages=find_packages(include=['asispectralinversion', 'asispectralinversion.*'])
 )
